120bf6183b35b1282f7d71d347519fb66162682d
Sandeep <sandeep@opstree.com>
2022-11-25 16:59:39 +0530

    Added check for file exists

--- build.sh
+++ build.sh
@@ -15,6 +15,10 @@ elif [ `isStrNonEmpty ${FILE_TO_BE_UPLOADED}` -ne 0 ]
 then
     TASK_STATUS=1
     logErrorMessage "File to be uploaded not provided please check"
+elif [ `isFileExist ${FILE_TO_BE_UPLOADED}` -ne 0 ]
+then
+    TASK_STATUS=1
+    logErrorMessage "File to be uploaded does not exists please check"
 fi     
 
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
