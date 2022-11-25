a14f00e8054c8c6eb433998a52948b11aaedd56f
Sandeep <sandeep@opstree.com>
2022-11-25 16:42:40 +0530

    Trying to do valiation if empty value passed

--- build.sh
+++ build.sh
@@ -1,29 +1,16 @@
 #!/bin/bash
 source /opt/buildpiper/shell-functions/functions.sh
 source /opt/buildpiper/shell-functions/log-functions.sh
+source /opt/buildpiper/shell-functions/str-functions.sh
+source /opt/buildpiper/shell-functions/file-functions.sh
 
 sleep  "$SLEEP_DURATION"
-#logInfoMessage "I'll upload [$FILE] to the [$S3_BUCKET] bucket and have mounted at [${CODEBASE_DIR}/$FILE_DIR}]"
 
-if [ -f $FILE ]
-then 
-elif [ $S3_BUCKET ]
-else logInfoMessage "Failure."
-fi
-fi
+TASK_STATUS=$(( isStrNonEmpty $S3_BUCKET))
 
+if [ `isStrNonEmpty $S3_BUCKET` -ne 0 ]
+then
+    logErrorMessage "S3 buckets details are not provided please check"
+fi     
 
-
-# if [ $? -eq 0 ]
-# then
-#     logInfoMessage "Congratulations file is uploaded successfully to s3 bucket!!!"
-#     generateOutput ${ACTIVITY_SUB_TASK_CODE} build true "Congratulations ami created sucessfully "
-# elif [ "${VALIDATION_FAILURE_ACTION}" == "FAILURE" ]
-#     then
-#     logErrorMessage "Please check upload failed!!!"
-#     generateOutput ${ACTIVITY_SUB_TASK_CODE} false "Please check upload failed!!!"
-#     exit 1
-#     else
-#     logWarningMessage "Please check upload failed!!!"
-#     generateOutput ${ACTIVITY_SUB_TASK_CODE} true "Please check upload failed!!!"
-# fi 
+saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
