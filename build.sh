79f69311b070bf14b1dc90f1bb757a9c22d04ca4
Aakash Tripathi <aakash.tripathi@opstree.com>
2024-12-05 13:42:28 +0530

    modifying build.sh

--- build.sh
+++ build.sh
@@ -9,31 +9,10 @@ CODEBASE_LOCATION="${WORKSPACE}"/"${CODEBASE_DIR}"
 logInfoMessage "I'll do processing at [$CODEBASE_LOCATION]"
 sleep  $SLEEP_DURATION
 
-cd  "${CODEBASE_LOCATION}"
-
-TASK_STATUS=0
-
-if [ `isStrNonEmpty $S3_BUCKET` -ne 0 ]
-then
-    TASK_STATUS=1
-    logErrorMessage "S3 buckets details are not provided please check"
-elif [ `isStrNonEmpty ${FILE_TO_BE_UPLOADED}` -ne 0 ]
-then
-    TASK_STATUS=1
-    logErrorMessage "File to be uploaded not provided please check"
-elif [ `isFileExist ${FILE_TO_BE_UPLOADED}` -ne 0 ]
-then
-    TASK_STATUS=1
-    logErrorMessage "File to be uploaded does not exists please check"
-elif [ `bucketExist ${S3_BUCKET}` -ne 0 ]
-then
-    TASK_STATUS=1
-    logErrorMessage "Unable to access S3 bucket either it doesn't exist or relevant permissions are not given please check!!!!"
-elif [ `isStrNonEmpty ${FILE_KEY}` -ne 0 ]
-then
-    TASK_STATUS=1
-    logErrorMessage "File key is not provided please check!!!!"
-fi     
+cd  "${WORKSPACE}"
+TAG=$(tail -n 1 data.properties)
+FILE_TO_BE_UPLOADED="${CODEBASE_DIR}"-"${TAG}".zip
+FILE_KEY="${CODEBASE_DIR}"-"${TAG}".zip
 
 logInfoMessage "Received below arguments"
 logInfoMessage "File to be uploaded: ${FILE_TO_BE_UPLOADED}"
@@ -41,4 +20,6 @@ logInfoMessage "S3 Bucket: ${S3_BUCKET}"
 logInfoMessage "Location of file in S3 Bucket: ${FILE_KEY}"
 
 copyFileToS3 ${FILE_TO_BE_UPLOADED} ${S3_BUCKET} ${FILE_KEY}
+#aws s3 cp "${FILE_TO_BE_UPLOADED}" s3://"${S3_BUCKET}"/"${FILE_KEY}"
+TASK_STATUS=$?
 saveTaskStatus ${TASK_STATUS} ${ACTIVITY_SUB_TASK_CODE}
