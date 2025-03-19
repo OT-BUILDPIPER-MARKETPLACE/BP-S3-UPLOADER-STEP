40b1b6333f56052385cc0f4001c2d76ff03f94c2
HarshitDev-Ops <harshit.singh@opstree.com>
2025-03-19 22:35:18 +0530

    updated utilities/upload_json_components_to_s3.sh

--- utilities/upload_json_components_to_s3.sh
+++ utilities/upload_json_components_to_s3.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#!/bin/bash
 
 # Function to upload files to S3 bucket for all components in a JSON file
 upload_json_components_to_s3() {
@@ -31,21 +32,51 @@ upload_json_components_to_s3() {
         logInfoMessage "Uploading files for component '$component' to S3 bucket '$S3_BUCKET/$s3_app_folder_name'..."
         for file in $files; do
 
-            if [[ "$file" == "spark_config" ]]; then
-                if [[ -n "$ENV" ]]; then
-                    filename="${file}_${ENV}.json"
-                else
-                    filename="${file}.json"
-                fi
+            if [[ "$file" == "spark_config" || "$file" == "snowflake_config" ]];then
+                filename="${file}_${ENV}.json"
             else
                 filename="${file}"
             fi
 
-            aws s3 cp "${component}/$filename" "s3://${S3_BUCKET}/${s3_app_folder_name}/${filename}"
-            if [ $? -ne 0 ]; then
-                logWarningMessage "Failed to upload $filename"
-                warning_flag="1"
-                continue
+            if [[ "$ENV" == "dev" ]];then
+                if [[ "$file" == "snowflake_config" ]]; then
+                    aws s3 cp "${component}/$filename" "s3://${S3_BUCKET}/${FILE_KEY}/${s3_app_folder_name}/$snowflake_config.json"
+                    if [ $? -ne 0 ]; then
+                        logWarningMessage "Failed to upload $filename"
+                        warning_flag="1"
+                        continue
+                    fi
+                else
+                    aws s3 cp "${component}/$filename" "s3://${S3_BUCKET}/${FILE_KEY}/${s3_app_folder_name}/"
+                    if [ $? -ne 0 ]; then
+                        logWarningMessage "Failed to upload $filename"
+                        warning_flag="1"
+                        continue
+                    fi
+                fi
+            else
+                if [[ "$file" == "snowflake_config" ]]; then
+                    aws s3 cp "${component}/$filename" "s3://${S3_BUCKET}/${FILE_KEY}/${s3_app_folder_name}_${ENV}/$snowflake_config.json"
+                    if [ $? -ne 0 ]; then
+                        logWarningMessage "Failed to upload $filename"
+                        warning_flag="1"
+                        continue
+                    fi
+                elif [[ "$component" == "common_files" ]];then
+                    aws s3 cp "${component}/$filename" "s3://${S3_BUCKET}/${FILE_KEY}/${s3_app_folder_name}/"
+                    if [ $? -ne 0 ]; then
+                        logWarningMessage "Failed to upload $filename"
+                        warning_flag="1"
+                        continue
+                    fi
+                else
+                    aws s3 cp "${component}/$filename" "s3://${S3_BUCKET}/${FILE_KEY}/${s3_app_folder_name}_${ENV}/"
+                    if [ $? -ne 0 ]; then
+                        logWarningMessage "Failed to upload $filename"
+                        warning_flag="1"
+                        continue
+                    fi
+                fi
             fi
         done
 
@@ -53,7 +84,7 @@ upload_json_components_to_s3() {
         if [[ -n "$dag_files" ]]; then
             logInfoMessage "Uploading DAG files for component '$component' to S3 bucket '$S3_BUCKET/dags'..."
             for dag_file in $dag_files; do
-                aws s3 cp "${component}/$dag_file" "s3://$S3_BUCKET/$s3_app_folder_name/$dag_file"
+                aws s3 cp "${component}/$dag_file" "s3://$S3_BUCKET/${FILE_KEY}/dags/$dag_file"
                 if [ $? -ne 0 ]; then
                     logWarningMessage "Failed to upload $dag_file"
                     warning_flag="1"
