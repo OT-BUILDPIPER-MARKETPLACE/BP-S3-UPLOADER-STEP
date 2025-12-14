98ad3909b5e1e7aeb7d5242d6f0b35bcd94818c6
Aayush Verma <aayush.verma@opstree.com>
2025-12-14 15:27:54 +0530

    update shell funtion

--- BP-BASE-SHELL-STEPS/aws-functions.sh
+++ BP-BASE-SHELL-STEPS/aws-functions.sh
@@ -60,14 +60,12 @@ function createRole() {
 }
 
 function getAssumeRole() {
-    ACCOUNT_ID=$1
-    ROLE_NAME=$2
+    ROLE_ARN=$1
 
     export $(printf "AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s" \
 	$(aws sts assume-role \
-	--role-arn arn:aws:iam::$ACCOUNT_ID:role/$ROLE_NAME \
+	--role-arn ${ROLE_ARN} \
 	--role-session-name default \
 	--query "Credentials.[AccessKeyId,SecretAccessKey,SessionToken]" \
 	--output text))
 }
-
--- BP-BASE-SHELL-STEPS/git-functions.sh
+++ BP-BASE-SHELL-STEPS/git-functions.sh
@@ -14,6 +14,7 @@ function findConflictingFiles() {
     SRC_BRANCH="$1"
     TGT_BRANCH="$2"
 
+    git checkout -q "${SRC_BRANCH}"
     git checkout -q "${TGT_BRANCH}"
     # git pull origin "${TGT_BRANCH}"
 
@@ -21,7 +22,9 @@ function findConflictingFiles() {
     git merge -q "$SRC_BRANCH" 1> /dev/null
 
     conflicts=$(git diff --name-only --diff-filter=U)
-    git merge --abort
+    if [ -n "${conflicts}" ]; then
+        git merge --abort
+    fi
     git checkout -q "${TGT_BRANCH}"
     git branch -q -D temp_merge_branch
     echo "$conflicts"
@@ -36,12 +39,44 @@ function getLastAuthorOfFile() {
     git log -1 --pretty=format:"%an" "${FILE}"
 }
 
-function getLastAuthorOfFiles() {
-    BRANCH="$1"
-    FILES="$2"
-    for FILE in "${FILES[@]}"
+function listAuthorsOfFilesAcrossBranches() {
+    SRC_BRANCH="$1"
+    TGT_BRANCH="$2"
+    CONFLICTING_FILES="$3"
+    echo File,${SRC_BRANCH},${TGT_BRANCH} > fileAuthors.tmp
+    # echo "Conflicting files variable: [${CONFLICTING_FILES}]"
+    for FILE in ${CONFLICTING_FILES}
     do
-        author=$(getLastAuthorOfFile "$BRANCH" "$FILE")
-        echo "${FILE}: ${author}"
+        # echo "Processing file [${FILE}]"
+        src_branch_author=`getLastAuthorOfFile "${SRC_BRANCH}" "${FILE}"`
+        # echo "Source branch Author: [${src_branch_author}]"
+        tgt_branch_author=`getLastAuthorOfFile "${TGT_BRANCH}" "${FILE}"`
+        # echo "Target branch Author: [${tgt_branch_author}]"
+        echo "$FILE,${src_branch_author},${tgt_branch_author}" >> fileAuthors.tmp
     done
+
+    cat fileAuthors.tmp | csvlook
+}
+
+branch_exists() {
+  local branch_name="$1"
+  
+  # Check if the branch exists by using "git show-ref" and "grep"
+  if git show-ref --verify --quiet "refs/heads/$branch_name"; then
+    echo "Branch '$branch_name' exists."
+    return 0  # Branch exists
+  else
+    echo "Branch '$branch_name' does not exist."
+    return 1  # Branch does not exist
+  fi
+}
+
+# Capture Git Commit Hash
+capture_commit_hash() {
+  git log -n 1 --pretty=format:%H
+}
+
+# Capture Branch Name
+capture_branch_name() {
+  git rev-parse --abbrev-ref HEAD
 }
